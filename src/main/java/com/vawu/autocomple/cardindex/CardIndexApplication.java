856cc434982b62fdafd9625b2a271a3dc379a1da
vawu <2064755975@qq.com>
2020-09-26 12:41:49 +0800

    update

--- src/main/java/com/vawu/autocomple/cardindex/CardIndexApplication.java
+++ src/main/java/com/vawu/autocomple/cardindex/CardIndexApplication.java
@@ -58,7 +58,7 @@ public class CardIndexApplication {
         private void configureTasks() {
             HttpResponse result2 = HttpRequest.post(timeConfig.getUrls().get(sed.getSed()))
                     .header(Header.USER_AGENT, timeConfig.getMy_headers()[RandomUtil.randomInt(0, 10)])//头信息，多个头信息多次调用此方法即可
-                    .timeout(60000)//超时，毫秒
+                    .timeout(120000)//超时，毫秒
                     .execute();
             log.info("开始打卡-------->时间:" + sed.getSed());
             log.info("详细地址:"+utils.getAddress());
